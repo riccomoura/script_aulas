0a99f7c29a0999bb45e7d864e53029974c054118
Henrique Moura de Mesquita <moura.henrique@outlook.com>
2021-12-03 11:03:53 -0300

    Add files via upload

---
+++ ex112/utilidadescev/moeda/__init__.py
@@ -0,0 +1,33 @@
+def aumentar(preco=0, taxa=0, format=False):
+    res = preco + (preco * taxa / 100)
+    return res if format is False else moeda(res)
+
+
+def diminuir(preco=0, taxa=0, format=False):
+    res = preco - (preco * taxa / 100)
+    return res if format is False else moeda(res)
+
+def dobro(preco=0, format=False):
+    res = preco * 2
+    return res if not format else moeda(res)
+
+
+def metade(preco=0, format=False):
+    res = preco / 2
+    return res if not format else moeda(res)
+
+
+def moeda(preco=0, moeda='R$'):
+    return f'{moeda}{preco:8.2f}'.replace('.', ',')
+
+
+def resumo(preco=0, taxaa=0, taxar=0):
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'Com {taxaa}% de aumento:\t{aumentar(preco, taxaa, True)}')
+    print(f'Com {taxar}% de redução:\t{diminuir(preco, taxar, True)}')
+    print('-' * 30)
